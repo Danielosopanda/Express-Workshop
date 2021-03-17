cd3e123b16b2e20f8463efb0a206cc8177967e80
Danielosopanda <danielmtzgro@hotmail.com>
2021-03-16 20:40:36 -0600

    Added Pokédex

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
+const { request, response } = require("express");
 const express = require("express");
 const app = express();
+const {pokemon} = require("./pokedex.json");
 
 app.get("/", (request, response, next) => {
-    response.status(200);
-    response.send("Hola mundo");
+    response.send("Bienvenido a la Pokédex");
 });
 
-app.listen(3000, () => {
+app.get("/pokemon", (request, response, next) => {
+    response.status(200);
+    response.send(pokemon);
+});
+
+app.get("/pokemon/all", (request, response, next) => {
+    response.status(200);
+    response.send(pokemon);
+});
+
+
+app.get("/pokemon/:id([0-9]{1,3})", (request, response, next) => {
+    const id = request.params.id - 1;
+    if(id >= 0 && id <= 150){
+        response.status(200);
+        response.send(pokemon[request.params.id - 1]);  
+    } else {
+        response.status(404);
+        response.send("Pokémon no encontrado");
+    }
+});
+
+app.get("/pokemon/:name", (request, response, next) => {
+    const name = request.params.name;
+    
+    pokemon.forEach(pokemon => {
+        if(pokemon.name == name){
+            response.status(200);
+            response.send(pokemon);
+        }
+        response.status(404);
+        response.send("Pokémon no encontrado");
+    });
+});
+
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running");
 });
 
