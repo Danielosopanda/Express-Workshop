de891ad5490a41303dd53393ac3527b46d0adc97
Danielosopanda <danielmtzgro@hotmail.com>
2021-03-18 17:47:50 -0600

    Added POST method for /pokemon

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const { request, response } = require("express");
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const {pokemon} = require("./pokedex.json");
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 //Página principal
 app.get("/", (request, response, next) => {
     return response.status(200).send("Bienvenido a la Pokédex");
+});
+
+app.post("/pokemon", (request, response, next) => {
+    return response.status(200).send(request.body)
 });
 
 // /pokemon (mostrar todos los pokémon)
@@ -29,7 +36,7 @@ app.get("/pokemon/:name([A-Za-z]+)", (request, response, next) => {
     const name = request.params.name;
 
     const pk = pokemon.filter((pokemon) => {
-        return (pokemon.name.toUpperCase() == name.toUpperCase()) ? pokemon : null;
+        return (pokemon.name.toUpperCase() == name.toUpperCase()) && pokemon;
     });
 
     (pk.length > 0) ? response.status(200).send(pk) : response.status(400).send("Pokémon no encontrado");
