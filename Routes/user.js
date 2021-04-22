1cf806a2a933ae9716ad5adf31fecff4ad5e65e1
Danielosopanda <danielmtzgro@hotmail.com>
2021-04-22 18:24:11 -0500

    Added JWT

--- Routes/user.js
+++ Routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const user = express.Router();
+const jwt = require("jsonwebtoken");
 const db = require("../config/database");
 
 user.post("/", async (request, response, next) => {
@@ -23,12 +24,37 @@ user.post("/", async (request, response, next) => {
     
 });
 
+user.post("/login", async (request, response, next) => {
+
+    const { user_mail, user_password } = request.body;
+    const query = `SELECT * FROM user WHERE user_mail = '${user_mail}' AND user_password = '${user_password}'`;
+
+    const rows = await db.query(query);
+
+    if(user_mail && user_password) {
+        if(rows.length == 1) {
+
+            const token = jwt.sign({
+                user_id: rows[0].user_id, 
+                user_mail: rows[0].user_mail
+            }, "debugkey");
+
+            return response.status(200).json({ code: 200, message: token });
+        } else {
+            return response.status(401).json({ code: 401, message: "El usuario y la contraseña no coinciden" });
+        }
+    }
+    return response.status(500).json({ code: 500, message: "Campos incompletos" });
+
+    
+});
+
 user.get("/", async (request, response, next) => {
 
     const query = "SELECT * FROM user";
     const rows = await db.query(query);
 
-    return response.status(200).json({ code: 201, message: rows });
+    return response.status(200).json({ code: 201, message: rows  });
 
 });
 
